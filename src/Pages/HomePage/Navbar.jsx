875c60b70c5929b2ee3c1c1c3afbaa91bd2bfbf1
Master_shiv.info.me <103109048+shivamkumarsha@users.noreply.github.com>
2024-06-01 18:23:29 +0530

    navbar

--- src/Pages/HomePage/Navbar.jsx
+++ src/Pages/HomePage/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { AiOutlineSun } from "react-icons/ai";
+import {
+  AiOutlineScan,
+  AiOutlineHome,
+  AiOutlineShoppingCart,
+  AiOutlineTags,
+} from "react-icons/ai";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 import { useCart } from "../../ContextApi/CartContextProvider";
 
 const Navbar = () => {
@@ -15,12 +21,20 @@ const Navbar = () => {
     <div className="fixed bottom-0 w-full z-50">
       <div className="md:hidden fixed bottom-0 left-0 w-full bg-black rounded-t-2xl backdrop-filter backdrop-blur-lg bg-opacity-30 shadow-lg z-50">
         <div className="flex justify-around items-center p-2">
-          <div className="flex justify-start -translate-x-4 space-x-2 mr-10">
+          <div className="flex justify-between space-x-5 -translate-x-3 items-start   ">
+            <NavLink
+              to="/menu"
+              className="nav-link transform transition-transform duration-300 hover:scale-105 text-center hover:shadow-3xl hover:bg-[#ffa600]   flex flex-col justify-center items-center text-white ease-in-out"
+            >
+              <AiOutlineTags size={30} />
+              <h1>Special</h1>
+            </NavLink>
             <NavLink
               to="/"
-              className="nav-link transform transition-transform duration-300 hover:scale-105 hover:shadow-3xl hover:bg-[#ffa600] hover:pb-3 hover:rounded-t-2xl p-3 text-white ease-in-out"
+              className="nav-link transform transition-transform duration-300 hover:scale-105 text-center hover:shadow-3xl hover:bg-[#ffa600]   flex flex-col justify-center items-center text-white ease-in-out"
             >
-              Home
+              <AiOutlineHome size={30} />
+              <h1>Home</h1>
             </NavLink>
           </div>
 
@@ -29,19 +43,28 @@ const Navbar = () => {
               to="/menu"
               className="nav-link transform transition-transform duration-300 hover:scale-105 hover:shadow-3xl bg-[#ffa600] hover:bg-white border-white hover:border-[orange] hover:border hover:-translate-y-3 hover:rotate-180 -translate-y-1 p-3 rounded-full text-black ease-in-out"
             >
-              <AiOutlineSun size={30} />
+              <MdOutlineRestaurantMenu size={33} />
             </NavLink>
           </div>
 
-          <div className="relative flex justify-end space-x-4 ml-5 -translate-x-3">
+          <div className="flex space-x-5 -translate-x-3">
+            <a
+              href="https://www.google.com/search?q=Google+QR+Code+Scanner"
+              to="/"
+              className="nav-link transform transition-transform duration-300 hover:scale-105 text-center hover:shadow-3xl hover:bg-[#ffa600]   flex flex-col justify-center items-center text-white ease-in-out"
+            >
+              <AiOutlineScan size={30} />
+              <h1>Scan</h1>
+            </a>
             <NavLink
               to="/cart"
-              className="nav-link transform transition-transform duration-300 hover:scale-105 hover:shadow-3xl hover:bg-[#ffa600] hover:pb-3 hover:rounded-t-2xl p-3 text-white ease-in-out"
+              className="nav-link transform transition-transform duration-300 hover:scale-105 text-center hover:shadow-3xl hover:bg-[#ffa600]   flex flex-col justify-center items-center text-white ease-in-out"
               style={{ position: "relative" }}
             >
+              <AiOutlineShoppingCart size={30} />
               Cart
               {cartItems.length > 0 && (
-                <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+                <span className="absolute top-0 -right-4 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
                   {cartItems.length}
                 </span>
               )}
